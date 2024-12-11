879a110531cacb302354d3dc6de6868e075c1472
Yassine Baddi <yassinbaddi7@gmail.com>
2024-12-11 20:33:37 +0100

    new krie

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import React from 'react'
+
+export default function setupTests({title}:{title:string}) {
+  return (
+    <div>{title}</div>
+  )
+}
+
