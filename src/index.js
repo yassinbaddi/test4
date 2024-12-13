384011501be43c3b5b9bc4b521d8b7b6b050bdce
Yassine Baddi <yassinbaddi7@gmail.com>
2024-12-13 17:59:44 +0100

    frecd

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "@radix-ui/themes/styles.css";
+import { Theme } from '@radix-ui/themes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   
+   	<Theme>
+
+  
     <App />
+    </Theme>
   </React.StrictMode>
 );
 
